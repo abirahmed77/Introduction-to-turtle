1dde878d72552714751104212bc576b8adf8f1b5
Abir <abirgpx@gmail.com>
2025-02-12 17:02:19 +0600

    hi

---
+++ square.py
@@ -0,0 +1,19 @@
+import turtle
+
+turtle.Screen().bgcolor("lavender")
+turtle.Screen().setup(300,400)
+polygon = turtle.Turtle()
+
+polygon.forward(100)
+polygon.right(90)
+
+polygon.forward(100)
+polygon.right(90)
+
+polygon.forward(100)
+polygon.right(90)
+
+polygon.forward(100)
+polygon.right(90)
+
+turtle.done()
